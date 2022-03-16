163ddff439681cd98e29a058a0725772048b7794
Michael Rinderle <michael@sofdigital.net>
2022-03-15 21:56:40 -0500

    updated app.tsx and app.css

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,10 @@
 import axios from 'axios';
-import React from 'react';
 import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
 import './App.css';
+
 
 class App extends React.Component {
     state = {
@@ -22,12 +25,31 @@ class App extends React.Component {
 
     render() {
         return(
-           <div className="App">
-                <header className="App-header">
-                    GoodThings
-                </header>
-                {this.state.data}
-            </div> 
+            <Router>
+                <div className="App">
+                    <header className="App-header">
+                        <h1>GoodThings</h1>
+                        <ul>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="/register">Register</Link></li>
+                            <li><Link to="/login">Login</Link></li>                
+                        </ul>
+                    </header>   
+                    <main>
+                        <Route exact path="/">
+                            {this.state.data}
+                        </Route>
+                        <Switch>
+                              <Route path="/register">
+                                Register
+                              </Route>
+                              <Route path="/login">
+                                Login
+                              </Route>
+                        </Switch>
+                    </main>
+                </div>
+            </Router> 
         );
     }
 }
