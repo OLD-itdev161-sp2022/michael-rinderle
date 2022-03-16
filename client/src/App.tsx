fd985411125df004919b09540ad63f25a0542356
Michael Rinderle <michael@sofdigital.net>
2022-03-15 22:12:31 -0500

    Completed Activity 7

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,8 +2,9 @@ import axios from 'axios';
 import logo from './logo.svg';
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-
 import './App.css';
+import Register from './components/Register/Register';
+import Login from './components/Login/Login';
 
 
 class App extends React.Component {
@@ -40,12 +41,8 @@ class App extends React.Component {
                             {this.state.data}
                         </Route>
                         <Switch>
-                              <Route path="/register">
-                                Register
-                              </Route>
-                              <Route path="/login">
-                                Login
-                              </Route>
+                            <Route path="/register" component={Register} />
+                            <Route path="/login" component={Login} />
                         </Switch>
                     </main>
                 </div>
